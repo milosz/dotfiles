7f80111fdc5cc53ef5f2292a9f602aff1df7264f
Milosz Galazka <milosz@sleeplessbeastie.eu>
2021-06-06 02:16:28 +0200

    added zsh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -2,5 +2,12 @@
 
 sudo apt install exa
 sudo apt install fd-find
-sudo apt install zsh
+sudo apt install fzf
 sudo apt install htop
+sudo apt install git
+sudo apt install zsh
+
+mkdir ~/.zinit
+git clone --depth 1 https://github.com/zdharma/zinit.git ~/.zinit/bin
+
+sudo chsh --shell /bin/zsh milosz
